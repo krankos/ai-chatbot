6b3ea38d96612fc791b3013e640884ad1843eefb
krankos <bessaadkhalil@gmail.com>
2025-05-23 14:27:58 +0100

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,8 @@ import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'Compliance Copilot for SAMA',
+  description: 'Ask any question about Saudi financial regulation, any time, anywhere.'
 };
 
 export const viewport = {
